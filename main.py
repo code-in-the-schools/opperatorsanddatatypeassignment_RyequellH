974419fe54ef0fa8844c5c2e86ba7dfefaa9994d
CodingQuell <ryequellhunter@gmail.com>
2020-07-15 17:27:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+
+
+january_rainfall_2019 = 3.15
+febuary_rainfall_2019 = 3.64
+march_rainfall_2019 = 4.14
+april_rainfall_2019 = 4.74
+may_rainfall_2019 = 2.45
+june_rainfall_2019 = 3.67
+july_rainfall_2019 = 2.49
+august_rainfall_2019 = 3.26
+september_rainfall_2019 = 4.86
+october_rainfall_2019 = 5.0
+november_rainfall_2019 = 3.63
+december_rainfall_2019 = 3.04
+
+summer_rainfall_2019 = 2.89
+spring_rainfall_2019 = 4.73
+fall_rainfall_2019 = 1.67
+winter_rainfall_2019 = 5.0
+
+winter_rainfall_2019 = november_rainfall_2019 + december_rainfall_2019 + january_rainfall_2019 + febuary_rainfall_2019
+avg_winter_rainfall_2019 = winter_rainfall_2019/4
+
+print("The total winter rainfall is", winter_rainfall_2019, "inches")
+print("The average winter rainfall is", avg_winter_rainfall_2019, "inches")
